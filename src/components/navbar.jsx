95b4e77c5a82e83da847cb5a1d395db8d435e2c9
myroslavkosyk81 <myroslav.kosyk@ukr.net>
2024-04-15 15:57:51 +0300

    animated menu

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,9 @@
 "use client";
+import Image from "next/image";
 import Link from "next/link"
 import { useState } from "react";
 import NavLink from "./navLink";
+import {motion} from "framer-motion";
 const links = [
       {url: '/', title: 'Home'},
       {url: '/about', title: 'About'},
@@ -11,19 +13,71 @@ const links = [
    
 const Navbar = () => {
    const [open, setOpen] = useState(false);
+   const topVariants={
+      closed: {
+         rotate:0,
+      },
+      opened:{
+         rotate:45,
+         backgroundColor:"rgb(255,255,255)",
+      },
+   };
+   const centerVariants={
+      closed: {
+         opacity:1,
+      },
+      opened:{
+         opacity:0,
+      },
+   };
+   const bottomVariants={
+      closed:{
+         rotate:0,
+      },
+      opened: {
+         rotate:-45,
+         backgroundColor:"rgb(255,255,255)",
+      },
+   };
+   const listVariants={
+      closed: {
+         x: "100vw",
+      },
+      opened:{
+         x: 0,
+         transition: {
+            when: "beforeChildren",
+            staggerChildren: 0.3,
+         },
+      },
+   };
+   const listItemVariants = {
+      closed: {
+         x: -10,
+         opacity: 0,
+      },
+      opened: {
+         x: 0,
+         opacity: 1,
+      },
+   }
+
    return (
       <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
+         {/* links */}
          <div className="hidden md:flex gap-4 w-1/3">
             {links.map(link => (
                <NavLink link={link} key={link.title} />
             ))}
          </div>
+         {/* logo */}
          <div className="md:hidden lg:flex xl:w-1/3 xl:justify-center">
             <Link href="/" className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center">
                <span className="text-white mr-1">Myroslav</span>
                <span className="w-12 h-8 ronded bg-white text-black flex items-center justify-center">Kosyk</span>
             </Link>
          </div>
+         {/* social links */}
          <div className="hidden md:flex gap-4 w-1/3">
             <Link href='https://github.com/myroslavkosyk81'>
                <img src='/github.png' alt='github' width ={24} height={24} />
@@ -35,20 +89,25 @@ const Navbar = () => {
                <img src='/linkedin.png' alt='linkedin' width ={24} height={24} />
             </Link>
          </div>
+         {/* menu */}
          <div className="md:hidden">
             <button className="w-10 h-8 flex flex-col justify-between z-50 relative" onClick={()=>setOpen((prev) => !prev)}>
-               <div className="w-10 h-1 bg-white rounded"></div>
-               <div className="w-10 h-1 bg-white rounded"></div>
-               <div className="w-10 h-1 bg-white rounded"></div>
+               <motion.div variants={topVariants} animate={open? "opened" : "closed"} className="w-10 h-1 bg-black rounded origin-left"></motion.div>
+               <motion.div variants={centerVariants} animate={open? "opened" : "closed"} className="w-10 h-1 bg-black rounded"></motion.div>
+               <motion.div variants={bottomVariants} animate={open? "opened" : "closed"} className="w-10 h-1 bg-black rounded origin-left"></motion.div>
             </button>
+            
             {open && (
-               <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
+               <motion.div variants={listVariants} initial="closed" animate='opened' className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl z-40">
                {links.map((link) => (
-                  <Link href={link.url} key={link.title}>
-                     {link.title}
-                  </Link>
+                  <motion.div variants={listItemVariants} className="" key={link.title}>
+                     <Link href={link.url} >
+                        {link.title}
+                     </Link>
+                  </motion.div>
+                  
                ))}
-               </div>
+               </motion.div>
                )           
             }
             
